89732537249943a7f6382584c5856751e8b96c3f
mebishnu34 <96931699+mebishnu34@users.noreply.github.com>
2026-08-31 19:49:06 -0700

    replace

--- Object/update_teacher_application.php
+++ Object/update_teacher_application.php
@@ -1,22 +1,80 @@
 <?php
 session_start();
 error_reporting(E_ALL);
-ini_set('display_error',1);
+ini_set('display_errors',1);
 //include("object_include.php");
 include("../Processing/db_connection.php");
-$teachercode=$_POST["txtcode"];
-if($_POST['cmbprioritymode']=="अनलाइन")
-  {
-$mode2="आमनेसामने";
-  }
-else
-  {
-    $mode2="अनलाइन";
-  }
-$sql = "UPDATE tblapplication 
-              SET
-                tname='".$_POST['txtteacherName']."',
-                teachercode='".$teachercode."',
+/*
+$citizen=$_POST['txtcitizenshipNo'];
+$mobileno=$_POST['txtmobileNo'];
+$class=$_POST['cmbclass'];
+$filename = $_FILES['fileletter']['name'];
+$temp_file=$mobileno .'_' . $filename;
+$letter=$temp_file;
+$folderletter = '../application_document/' . $temp_file;
+$extensionletter = pathinfo($filename, PATHINFO_EXTENSION);
+$fileletter = $_FILES['fileletter']['tmp_name'];
+$sizeletter = $_FILES['fileletter']['size'];
+
+$filenamectz = $_FILES['filecitizenship']['name'];
+$temp_file=$mobileno .'_' . $filenamectz;
+$citizenship=$temp_file;
+$folderctz = '../application_document/' . $temp_file;
+$extensionctz = pathinfo($filenamectz, PATHINFO_EXTENSION);
+$filectz = $_FILES['filecitizenship']['tmp_name'];
+$sizectz = $_FILES['filecitizenship']['size'];
+
+$filenamephoto = $_FILES['filephoto']['name'];
+$temp_file=$mobileno .'_' . $filenamephoto;
+$photo=$temp_file;
+$folderphoto= '../application_document/' . $temp_file;
+$extensionphoto = pathinfo($filenamephoto, PATHINFO_EXTENSION);
+$filephoto = $_FILES['filephoto']['tmp_name'];
+$sizephoto = $_FILES['filephoto']['size'];
+$recommend="";
+
+     if(isset($_FILES['fileschoolrecommend']))
+        {
+          $filenamerecomend = $_FILES['fileschoolrecommend']['name'];
+          $temp_file=$mobileno .'_' . $filenamerecomend;
+          $recommend=$temp_file;
+          $folderrecomend = '../application_document/' . $temp_file;
+          $extensionrecomend = pathinfo($filenamerecomend, PATHINFO_EXTENSION);
+          $filerecomend = $_FILES['fileschoolrecommend']['tmp_name'];
+          $sizerecomend = $_FILES['fileschoolrecommend']['size'];
+          if (!in_array($extensionrecomend, ['PDF','pdf','Pdf']))
+            {
+              header('Location: ../error.php?msg= "Recomended file must be PDF"');
+            }
+          elseif ($sizerecomend > 5000000)
+            { // file shouldn't be larger than 1Megabyte
+             header('Location: ../error.php?msg= "File to large"');
+    	      }
+          else
+            {
+              copy($filerecomend,$folderrecomend);
+            }
+        }
+      if (!in_array($extensionletter, ['PDF','pdf','Pdf']) OR !in_array($extensionctz, ['PDF', 'pdf','Pdf']) OR !in_array($extensionphoto, ['JPG','jpg','jpeg','JPEG','Jpg','Jpeg','PNG','png','Png']))
+        {
+          header('Location: ../error.php?msg= "Your file extension must be PDF"');
+        }
+      elseif ($sizeletter > 5000000 OR $sizectz>5000000 OR $sizephoto>5000000)
+        { // file shouldn't be larger than 1Megabyte
+          header('Location: ../error.php?msg= "File to large"');
+    	  }
+      else
+        {
+          if (copy($fileletter, $folderletter) and copy($filectz, $folderctz) and copy($filephoto,$folderphoto))
+            {
+    */
+              $sql = "UPDATE tblapplication set 
+              tname='".$_POST['txtteacherName']."',
+                teachercode='0',
+          		  runtrainingid='0',
+		            groupnumber='0',
+                gender='".$_POST['optgender']."',
+                teacherdob='".$_POST['txtdob']."',
                 fathername='".$_POST['txtfatherName']."',
                 province='".$_POST['cmbprovince']."',
                 district='".$_POST['cmbdistrictnp']."',
@@ -24,129 +82,38 @@ $sql = "UPDATE tblapplication
                 wardno='".$_POST['txtward']."',
                 mobileno='".$_POST['txtmobileNo']."',
                 email='".$_POST['txtemail']."',
+                citizenshipno='".$_POST['txtcitizenshipNo']."',
                 issuedistrict='".$_POST['cmbctzissuedistrict']."',
                 appointdate='".$_POST['txtappointdate']."',
                 appointmonth='".$_POST['cmbappointmonth']."',
                 appointday='".$_POST['txtday']."',
+                praappointdate='".$_POST['txtpraappointmiti']."',
                 appointdistrict='".$_POST['cmbappointdistrict']."',
                 appointlocallevel='".$_POST['cmbappointlevel']."',
                 appointsubject='".$_POST['cmbappointsubject']."',
                 bankname='".$_POST['txtbankname']."',
+                acholdername='".$_POST['txtaccountholder']."',
                 bankacno='".$_POST['txtbankacno']."',
                 panno='".$_POST['txtpanNo']."',
+                teacherclass='".$class."',
                 schoolname='".$_POST['txtschoolname']."',
                 schoolprovince='".$_POST['cmbschoolprovince']."',
                 schooldistrict='".$_POST['cmbdistrictbagamati']."',
                 schoollocallevel='".$_POST['cmbmunbagamati']."',
-                schoolward='".$_POST['txtschoolward']."',
-                trainingcategory='".$_POST['cmbtrainingcategory']."',
-                trainingsubject='".$_POST['cmbsubject']."',
-                priority1model='".$_POST['cmbprioritymode']."',
-                priority2model='".$mode2."'
-                where citizenshipno='".$_POST['txtcitizenshipNo']."' and financialyear='".$_POST['txtfyear']."'";
+                schoolward='".$_POST['txtschoolward']."'
+                where appid='".$_POST['txtapplicationid']."'";
                 if (mysqli_query($conn, $sql))
                   {
-                        $filename = $_FILES['fileletter']['name'];
-                    $temp_file=$citizen .'_' . $filename;
-                    $letter=$temp_file;
-                    $folderletter = '../application_document/' . $temp_file;
-                    $extensionletter = pathinfo($filename, PATHINFO_EXTENSION);
-                    $fileletter = $_FILES['fileletter']['tmp_name'];
-                    $sizeletter = $_FILES['fileletter']['size'];
-                    if($filename<>"")
-                      {
-                        if (!in_array($extensionletter, ['PDF', 'JPG','png', 'PNG','pdf','jpg','jpeg']))
-                          {
-                            header('Location: ../error.php?msg= "Your file extension must be PDF, JPG वा PNG (Max 5MB"');
-                            }
-                        elseif ($sizeletter > 5000000)
-                          { // file shouldn't be larger than 1Megabyte
-                            header('Location: ../error.php?msg= "File to large"');
-                          }
-                          else
-                            {
-                              if(copy($fileletter, $folderletter))
-                                {
-                                  mysqli_query($conn,"Update tblapplication set  appointletter='".$letter."' where teachercode='".$teachercode."'");
-                                }
-                            }
-                      }
-
-                      $filenamectz = $_FILES['filecitizenship']['name'];
-                      $temp_file=$citizen .'_' . $filenamectz;
-                      $citizenship=$temp_file;
-                      $folderctz = '../application_document/' . $temp_file;
-                      $extensionctz = pathinfo($filenamectz, PATHINFO_EXTENSION);
-                      $filectz = $_FILES['filecitizenship']['tmp_name'];
-                      $sizectz = $_FILES['filecitizenship']['size'];
-
-                    if($filenamectz<>"")
-                      {
-                          if(!in_array($extensionctz, ['PDF', 'JPG','png', 'PNG','pdf','jpg','jpeg']))
-                            {
-                             header('Location: ../error.php?msg= "Your file extension must be PDF, JPG वा PNG (Max 5MB"');
-                            }
-                        elseif ($sizectz>5000000)
-                              { // file shouldn't be larger than 1Megabyte
-                                header('Location: ../error.php?msg= "File to large"');
-                              }
-                          else
-                            {
-                              if(copy($filectz, $folderctz))
-                                {
-                                  mysqli_query($conn,"Update tblapplication set  citizenship='".$citizenship."' where teachercode='".$teachercode."'");
-                                }
-                            }
-                      }
-                      $filenamerecomend = $_FILES['fileschoolrecommend']['name'];
-                      $temp_file=$citizen .'_' . $filenamerecomend;
-                      $recommend=$temp_file;
-                      $folderrecomend = '../application_document/' . $temp_file;
-                      $extensionrecomend = pathinfo($filenamerecomend, PATHINFO_EXTENSION);
-                      $filerecomend = $_FILES['fileschoolrecommend']['tmp_name'];
-                      $sizerecomend = $_FILES['fileschoolrecommend']['size'];               
-                    if($filenamectz<>"")
-                        {
-                          if(!in_array($extensionrecomend, ['PDF', 'JPG','png', 'PNG','pdf','jpg','jpeg']))
-                            {
-                              header('Location: ../error.php?msg= "Your file extension must be PDF, JPG वा PNG (Max 5MB"');
-                            }
-                          elseif ($sizerecomend>5000000)
-                            { // file shouldn't be larger than 1Megabyte
-                              header('Location: ../error.php?msg= "File to large"');
-                            }
-                          else
-                            {
-                            if(copy($filerecomend,$folderrecomend))
-                              {
-                                mysqli_query($conn,"Update tblapplication set  schoolrecommend='".$recommend."' where teachercode='".$teachercode."'");
-                              }
-                            }
-                        }
-                        mysqli_query($conn,"UPDATE tblteacher t1
-								        JOIN tblapplication t2 
-								        ON (t1.teachercode = t2.teachercode)
-								            SET
-                            t1.tname = t2.tname,
-                            t1.gender = t2.gender,
-                            t1.citizenship = t2.citizenshipno,
-                            t1.fathername = t2.fathername,
-                            t1.email = t2.email,
-                            t1.wardno = t2.wardno,
-                            t1.tcontact = t2.mobileno,
-                            t1.appointdate = t2.appointdate,
-                            t1.subject = t2.appointsubject,
-                            t1.teachinglevel = t2.appointsubject,
-                            t1.teachingsubject = t2.appointsubject,
-                            t1.loginname = t2.mobileno,
-                            t1.tpass = t2.mobileno");
-                 		    $_SESSION['response']="Update Successfully";
-                        header('Location: ../application.php');
+			              $_SESSION['response']="Update Successfully";
+                    ?>
+                    <script>
+                      window.close();
+                    </script>
+                    <?php
                   }
-            else
+                else
                   {
-                    //header('Location: ../error.php?msg='. die("Database Connection Error" .mysqli_error()));
-                    echo mysqli_error();
-                  } 
+                    header('Location: ../error.php?msg='. die("Database Connection Error" .mysqli_error()));
+                  }
 mysqli_close($conn);
 ?>
